099ff5d36082c10741ef837bb94fc8b4662da7a6
jubianchi <contact@jubianchi.fr>
2016-01-10 22:06:03 +0100

    Tweak configuration for PHP7 to avoid segfaults

--- .atoum.php
+++ .atoum.php
@@ -5,3 +5,11 @@ use mageekguy\atoum\phpunit;
 
 $extension = new phpunit\extension($script);
 $extension->addToRunner($runner);
+
+if (getenv('TRAVIS_PHP_VERSION') === '7.0')
+{
+    $script
+        ->php('php -n -ddate.timezone=Europe/Paris')
+        ->noCodeCoverage()
+    ;
+}
